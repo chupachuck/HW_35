7fd974196865c301463ad77a10daa9a61cd7cdfc
Chupa Chuck <garyisnottooold@gmail.com>
2019-07-07 00:44:19 -0700

    first commit

---
+++ src/main/java/core/HW_35/App.java
@@ -0,0 +1,13 @@
+package core.HW_35;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello W1orld!" );
+    }
+}
